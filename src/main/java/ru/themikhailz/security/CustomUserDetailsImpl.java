3119e1230a64566d11ad6ad3536729d818356ae7
apatios <apatrios@mail.ru>
2023-08-26 08:50:48 +1000

    builder for CustomUserDetails

--- src/main/java/ru/themikhailz/security/CustomUserDetailsImpl.java
+++ src/main/java/ru/themikhailz/security/CustomUserDetailsImpl.java
@@ -20,6 +20,17 @@ public class CustomUserDetailsImpl implements UserDetails {
     private boolean isCredentialsNonExpired;
     private boolean isEnabled;
 
+    private CustomUserDetailsImpl(UUID id, String username, String password, Role role, boolean isAccountNonExpired, boolean isAccountNonLocked, boolean isCredentialsNonExpired, boolean isEnabled) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.isAccountNonExpired = isAccountNonExpired;
+        this.isAccountNonLocked = isAccountNonLocked;
+        this.isCredentialsNonExpired = isCredentialsNonExpired;
+        this.isEnabled = isEnabled;
+    }
+
     public void setId(UUID id) {
         this.id = id;
     }
@@ -89,5 +100,65 @@ public class CustomUserDetailsImpl implements UserDetails {
     @Override
     public boolean isEnabled() {
         return isEnabled;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private UUID id;
+        private String username;
+        private String password;
+
+        private Role role;
+        private boolean isAccountNonExpired;
+        private boolean isAccountNonLocked;
+        private boolean isCredentialsNonExpired;
+        private boolean isEnabled;
+
+        public Builder id(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder role(Role role) {
+            this.role = role;
+            return this;
+        }
+
+        public Builder isAccountNonExpired(boolean accountNonExpired) {
+            isAccountNonExpired = accountNonExpired;
+            return this;
+        }
+
+        public Builder isAccountNonLocked(boolean accountNonLocked) {
+            isAccountNonLocked = accountNonLocked;
+            return this;
+        }
+
+        public Builder isCredentialsNonExpired(boolean credentialsNonExpired) {
+            isCredentialsNonExpired = credentialsNonExpired;
+            return this;
+        }
+
+        public Builder isEnabled(boolean enabled) {
+            isEnabled = enabled;
+            return this;
+        }
+
+        public CustomUserDetailsImpl build() {
+            return new CustomUserDetailsImpl(id, username, password, role, isAccountNonExpired, isAccountNonLocked, isCredentialsNonExpired, isEnabled);
+        }
     }
 }
